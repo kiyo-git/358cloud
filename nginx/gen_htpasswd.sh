7fb0d8653d23430fd6359219eed713f8e0d4caf1
kiyo-git <ito.cloud.free.enji@gmail.com>
2023-06-29 14:41:22 +0900

    branch変更

--- nginx/gen_htpasswd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-USER_NAME=pist6-admin-user
-PASSWD=pist6-admin-pwd
-CRYPTPASS=`openssl passwd -crypt ${PASSWD}`
-
-echo "${USER_NAME}:${CRYPTPASS}" >> /etc/nginx/.htpasswd
--- pist6/local_packages/veritrans-tgmdk/src/tgMdk/dto/MdkBaseDto.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-namespace tgMdk\dto;
-
-class MdkBaseDto { }
-
